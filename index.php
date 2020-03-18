da9f792bc598c5808a833641ef3c1af86fd6df16
pellu <jordanpelluard@gmail.com>
2020-03-18 20:42:57 +0100

    Formulaire indisponible pour ne pas enfreindre les indications du ministère de l'intérieur

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if(empty($_POST)){
     <div class="container">
       <div class="pull-center">
         <h1>Attestation de déplacement dérogatoire</h1>
-        <p>Remplissez tous les champs, signez et téléchargez le pdf</p>
+        <p style="display: none;">Remplissez tous les champs, signez et téléchargez le pdf</p>
       </div>
     </div>
   </header>
@@ -40,12 +40,13 @@ if(empty($_POST)){
   <div class="container" id="content">
     <div class="row">
       <div class="col-12 col-lg-6 col-center">
-        <div class="alert alert-danger"><a href="https://twitter.com/Place_Beauvau/status/1240236276416118784" target="_blank">Le ministère de l'intérieur indique dans un tweet du 18 mars à 12h19</a> que les attestations officielles imprimées et les attestations sur l’honneur rédigées sur papier libre sont les seules valables. <a href="https://www.interieur.gouv.fr/Actualites/L-actu-du-Ministere/Attestation-de-deplacement-derogatoire-et-justificatif-de-deplacement-professionnel" target="_blank">Télécharger le PDF officiel</a></div>
-        <div class="alert alert-info text-center">Les informations rentrées dans le formulaire ci-dessous ne sont pas gardées sur le serveur.</div>
-        <div class="alert alert-warning text-center">Le site ne fonctionne pas via un navigateur imbriqué style Facebook, Messenger, Discord... Il est conseillé d'utiliser Chrome ou Firefox.</div>
+        <div class="alert alert-danger"><a href="https://twitter.com/Place_Beauvau/status/1240236276416118784" target="_blank">Le ministère de l'intérieur indique dans un tweet du 18 mars à 12h19</a> que les attestations officielles imprimées et les attestations sur l’honneur rédigées sur papier libre sont les seules valables. <a href="https://www.interieur.gouv.fr/Actualites/L-actu-du-Ministere/Attestation-de-deplacement-derogatoire-et-justificatif-de-deplacement-professionnel" target="_blank">Télécharger le PDF officiel sur le site du ministère de l'intérieur</a></div>
+        <div class="alert alert-info text-center">Le formulaire n'est plus disponible pour ne pas enfreindre les indications du ministère de l'intérieur.</div>
+        <div class="alert alert-info text-center" style="display: none;">Les informations rentrées dans le formulaire ci-dessous ne sont pas gardées sur le serveur.</div>
+        <div class="alert alert-warning text-center" style="display: none;">Le site ne fonctionne pas via un navigateur imbriqué style Facebook, Messenger, Discord... Il est conseillé d'utiliser Chrome ou Firefox.</div>
       </div>
     </div>
-    <div class="row">
+    <div class="row" style="display: none;">
       <div class="col-12 col-lg-6 col-center">
         <p class="text-center" style="font-size: 14.5px;">En application de l'article 1<sup>er</sup> du décret du 16 mars 2020 portant réglementation des déplacements dans le cadre de la lutte contre la propagation du virus Covid-19.</p>
         <form class="needs-validation" action="" method="POST" novalidate>
